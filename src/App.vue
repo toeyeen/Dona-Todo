fa8b27203ae65e41ca9f943ce7d5b36aec670dde
toeyeen <toeyeen@github.com>
2023-02-03 03:15:21 +0100

    chore:  Project setup

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div bg="~ gray/50" my-auto of-hidden h-screen grid class="grid-cols-[320px_1fr] border border-red px-5 py-4">
+    <LeftPanel />
+  </div>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/LeftPanel.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div bg="~ white" my-auto rounded-lg px-3 h-full>
+    <ul>
+      <li>Home</li>
+      <li>Home</li>
+      <li>Home</li>
+      <li>Home</li>
+      <li>Home</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
