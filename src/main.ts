c2c37524c386b927275356b5a6730670e8df5943
toeyeen <toeyeen@github.com>
2023-02-05 01:10:38 +0100

    feat: Task add

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,8 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
-import Home from './pages/Home.vue'
-import About from './pages/About.vue'
+import router from './router'
 
 import '@unocss/reset/tailwind.css'
 import 'uno.css'
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home,
-    },
-    {
-      path: '/about',
-      name: 'About',
-      component: About,
-    },
-  ],
-})
 
 createApp(App).use(router).mount('#app')
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,75 @@
 <script setup lang="ts">
+const todo = ref('')
+const taskRef = ref<HTMLDivElement>()
 
+interface Group {
+  id: number | string
+  name: string
+  emoji?: string
+}
+
+interface Todo {
+  id: number | string
+  title: string
+  isCompleted: false
+  dueDate?: Date
+  group?: Group[]
+}
+
+const todos = ref([])
+const todoId = ref(0)
+
+const addTodo = (value: Todo) => {
+  todos.value.unshift(value)
+  todo.value = ''
+
+  // todoRef.value.focus()
+}
 </script>
 
 <template>
-  <div border="~ green-800">
+  <div>
     <p>Home Page</p>
+
+    <div>
+      <input
+        id=""
+        ref="taskRef" v-model="todo" placeholder="Write a new task" type="text" name="todo" px-2 text-black border border-blue @keyup.enter="addTodo({
+          id: todoId++,
+          title: todo,
+          isCompleted: false,
+          group: [{
+            id: todoId++,
+            name: 'Personal',
+          }],
+        })"
+      >
+      <button
+        @click="addTodo({
+          id: todoId++,
+          title: todo,
+          isCompleted: false,
+          group: [{
+            id: todoId++,
+            name: 'Personal',
+          }],
+        })"
+      >
+        Add
+      </button>
+    </div>
+
+    <ul>
+      <li
+        v-for="todo, idx in todos" :key="idx" :class="[todo.isCompleted ? 'line-through' : '',
+        ]"
+        @click="todo.isCompleted
+          = !todo.isCompleted
+        "
+      >
+        {{ todo }}
+      </li>
+    </ul>
   </div>
 </template>
 
