9a8f5ad76822b04219a2e756dcd0ece64e720796
toeyeen <toeyeen@github.com>
2023-02-05 01:11:06 +0100

    chore: vue-router refactor

---
+++ src/router/index.ts
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('@/pages/Home.vue'),
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import('@/pages/About.vue'),
+  },
+
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
