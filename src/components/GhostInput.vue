b23c0427494286319d0b505782a4202323df0c4c
toeyeen <toeyeen@github.com>
2023-02-17 04:04:21 +0100

    style: Add custom component

---
+++ src/components/GhostInput.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+const props = defineProps({
+  placeholder: String,
+  disabled: Boolean,
+  modelValue: {
+    type: [String, Number],
+    default: '',
+  },
+  type: {
+    type: String,
+    default: 'text',
+  },
+  addonBefore: String,
+  addonAfter: String,
+})
+
+const emit = defineEmits(['update:modelValue', 'delete'])
+
+const updateValue = (e: Event) => {
+  const target = e.target as HTMLInputElement
+
+  emit('update:modelValue', target.value)
+}
+</script>
+
+<template>
+  <div />
+  <input type="text" class="ghost" :placeholder="placeholder
+  " :value="modelValue" v-bind="{
+  ...$attrs,
+  onInput: updateValue,
+}">
+</template>
+
+<style lang="scss" scoped>
+.ghost {
+  outline: none;
+
+  &:focus,
+  &:active {
+    outline: none;
+  }
+
+}
+</style>
